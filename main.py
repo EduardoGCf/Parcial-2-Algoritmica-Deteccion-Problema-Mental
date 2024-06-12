5638edceaeaa9abf183aa6b275482a18fc1686b2
EduardoGCf <gilcespedesfabianeduardo@gmail.com>
2024-06-12 16:19:22 -0400

    Avance practico: se arreglo los ultimos errores y el modelo esta entrenado y podemos solicitar el diagnostico de un paciente para saber cual es la categoria en la que entra de depresion

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-import joblib  # Para guardar y cargar el modelo
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.impute import SimpleImputer
+import joblib
 
 # Cargar los datos desde el archivo CSV
 df = pd.read_csv('Deepression.csv')
 
-# Mostrar las primeras filas del DataFrame
-print("Primeras filas del DataFrame:")
-print(df.head())
-
-# Mostrar información resumida del DataFrame
-print("\nDescripción del DataFrame:")
-print(df.describe(include='all'))
-
-# Imprimir los nombres de las columnas para diagnóstico
-print("\nNombres de las columnas del DataFrame:")
-print(df.columns)
-
 # Eliminar columnas 'Number' y 'Depression State'
-try:
-    # Corregir el nombre de la columna eliminando el espacio al final
-    df = df.rename(columns=lambda x: x.strip())
-    
-    # Eliminar columnas 'Number' y 'Depression State'
-    X = df.drop(['Number', 'Depression State'], axis=1)  # Características
-    y = df['Depression State']  # Etiquetas
-    
-    print("\nColumnas 'Number' y 'Depression State' eliminadas con éxito.")
-except KeyError as e:
-    print(f"\nError al intentar eliminar columnas: {e}")
+df = df.rename(columns=lambda x: x.strip())  # Corregir el nombre de la columna eliminando el espacio al final
+X = df.drop(['Number', 'Depression State'], axis=1)  # Características
+y = df['Depression State']  # Etiquetas
 
-# Codificar la columna de etiquetas antes de dividir los datos
+# Codificar la columna de etiquetas
 label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(df['Depression State'])  # Codificar las etiquetas de texto a números
+y_encoded = label_encoder.fit_transform(y)  # Codificar las etiquetas de texto a números
 
 # Escalar las características
 scaler = StandardScaler()
@@ -44,35 +25,43 @@ X_scaled = scaler.fit_transform(X)
 # Dividir los datos en conjuntos de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_encoded, test_size=0.2, random_state=42)
 
-# Entrenar el modelo utilizando las etiquetas codificadas
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+# Imputación de valores faltantes en el conjunto de entrenamiento
+imputer = SimpleImputer(strategy='mean')
+X_train_imputed = imputer.fit_transform(X_train)
+
+# Ajustar el modelo a los datos de entrenamiento
+knn_model = KNeighborsClassifier(n_neighbors=10)
+knn_model.fit(X_train_imputed, y_train)
+
+# Imputación de valores faltantes en el conjunto de prueba
+X_test_imputed = imputer.transform(X_test)
+
+# Hacer predicciones en el conjunto de prueba
+y_pred = knn_model.predict(X_test_imputed)
 
 # Guardar el modelo y el scaler
-joblib.dump(model, 'depression_model.pkl')
+joblib.dump(knn_model, 'depression_knn_model.pkl')
 joblib.dump(scaler, 'scaler.pkl')
 joblib.dump(label_encoder, 'label_encoder.pkl')
 
 print("\nModelo y scaler guardados exitosamente.")
 
-# Hacer predicciones en el conjunto de prueba
-y_pred = model.predict(X_test)
+# Obtener las clases únicas del conjunto de datos
+unique_classes = label_encoder.classes_
 
-# Convertir las etiquetas a cadenas de texto
-label_encoder_classes_str = [str(label) for label in label_encoder.classes_]
+# Convertir los nombres de las clases a cadenas
+unique_classes_str = [str(cls) for cls in unique_classes]
 
 # Evaluar el rendimiento del modelo
-report = classification_report(y_test, y_pred, labels=label_encoder.transform(label_encoder.classes_), target_names=label_encoder_classes_str)
-
+report = classification_report(y_test, y_pred, labels=range(len(unique_classes)), target_names=unique_classes_str)
 print("\nReporte de clasificación:")
 print(report)
 
 
-
 # Función para diagnosticar a nuevos usuarios
-def diagnosticar_usuario(nuevas_caracteristicas):
+def diagnosticar_usuario_knn(nuevas_caracteristicas):
     # Cargar el modelo entrenado, el scaler y el label encoder
-    modelo = joblib.load('depression_model.pkl')
+    modelo = joblib.load('depression_knn_model.pkl')
     escalador = joblib.load('scaler.pkl')
     codificador = joblib.load('label_encoder.pkl')
     
@@ -88,7 +77,20 @@ def diagnosticar_usuario(nuevas_caracteristicas):
     return estado_depresion[0]
 
 # Ejemplo de uso:
-#tabla de nombre de las caracteristicas
-nuevas_caracteristicas = [2, 3, 4, 5, 1, 3, 4, 5, 2, 1, 3, 4, 2, 5]  # Ejemplo de características del usuario
-diagnostico = diagnosticar_usuario(nuevas_caracteristicas)
+Sleep = 1  # Rarely
+Appetite = 3  # Often
+Interest = 2  # Always
+Fatigue = 4  # Rarely
+Worthlessness = 5  # Sometimes
+Concentration = 3  # Often
+Agitation = 2  # Always
+Suicidal_Ideation = 5  # Never
+Sleep_Disturbance = 5  # Rarely
+Aggression = 1  # Never
+Panic_Attacks = 3  # Never
+Hopelessness = 4  # Rarely
+Restlessness = 5  # Sometimes
+Low_Energy = 4  # Rarely
+nuevas_caracteristicas = [Sleep, Appetite, Interest, Fatigue, Worthlessness, Concentration, Agitation, Suicidal_Ideation, Sleep_Disturbance, Aggression, Panic_Attacks, Hopelessness, Restlessness, Low_Energy]  # Ejemplo de características del usuario
+diagnostico = diagnosticar_usuario_knn(nuevas_caracteristicas)
 print("\nDiagnóstico del usuario:", diagnostico)
